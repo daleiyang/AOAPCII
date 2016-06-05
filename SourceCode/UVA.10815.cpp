ffa10acfe9d551724d4a076f08e211f569edde34
Dalei Yang <dalya@microsoft.com>
2016-06-06 00:38:17 +0800

    2016.06.06

--- SourceCode/UVA.10815.cpp
+++ SourceCode/UVA.10815.cpp
@@ -2,24 +2,20 @@
 #include<iostream>
 #include<string>
 #include<set>
-#include<sstream>
 using namespace std;
-
 set<string> dict;
-
 int main(){
-	ios::sync_with_stdio(false);
 #ifdef LOCAL
-	freopen("UVA.10815.in", "r", stdin);
-	freopen("UVA.10815.out", "w", stdout);
+		freopen("UVA.10815.in", "r", stdin);
+		freopen("UVA.10815.out", "w", stdout);
 #endif
-	string s, buf;
-	while(cin>>s){
-		for(unsigned int i=0; i<s.length(); i++)
-			if(isalpha(s[i])) s[i]=tolower(s[i]);
-			else s[i]=' ';
-		stringstream ss(s);
-		while(ss>>buf) dict.insert(buf);
+	string s;
+	char c;
+	while((c=getchar()) != EOF){
+		if(!isalpha(c)){
+			if(s.size()){dict.insert(s); s.erase();}
+		}
+		else s.push_back(tolower(c));
 	}
 	for(set<string>::iterator it = dict.begin(); it != dict.end(); ++it)
 		cout<<*it<<"\n";
