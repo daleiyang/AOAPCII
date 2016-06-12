f6ae338773176a2c794f1ce734422943274d9614
Dalei Yang <dalya@microsoft.com>
2016-06-13 01:20:58 +0800

    2016.06.13

---
+++ SourceCode/UVA.221.cpp
@@ -0,0 +1,60 @@
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+
+const int maxn = 100 + 5;
+
+struct Building{
+	int id;
+	double x, y, w, d, h;
+	bool operator < (const Building & rhs) const{
+		return x < rhs.x || (x == rhs.x && y < rhs.y);
+	}
+} b[maxn];
+
+int n;
+double x[maxn*2];
+
+bool cover(int i, double mx){
+	return b[i].x <= mx && b[i].x + b[i].w >= mx;
+}
+
+bool visible(int i, double mx){
+	if(!cover(i, mx)) return false;
+	for(int j = 0; j < n; j++){
+		if(b[j].y < b[i].y && b[j].h >= b[i].h && cover(j, mx)) return false;
+	}
+	return true;
+}
+
+int main(){
+#ifdef LOCAL
+	freopen("UVA.221.in", "r", stdin);
+	freopen("UVA.221.out", "w", stdout);
+#endif
+
+	int kase = 0;
+	while(scanf("%d", &n) == 1 && n){
+		for(int i = 0; i < n; i++){
+			scanf("%lf%lf%lf%lf%lf", &b[i].x, &b[i].y, &b[i].w, &b[i].d, &b[i].h);
+			x[i * 2] = b[i].x;
+			x[i * 2 + 1] = b[i].x + b[i].w;
+			b[i].id = i + 1;
+		}
+		sort(b, b + n);
+		sort(x, x + maxn * 2);
+		int m = unique(x, x + maxn * 2) - x;
+		if(kase++) printf("\n");
+		printf("For map #%d, the visible buildings are numbered as follows:\n%d", kase, b[0].id);
+		for(int i = 1; i < n; i++){
+			bool vis = false;
+			for(int j= 0; j < m -1; j++){
+				if(visible(i, (x[j] + x[j + 1]) / 2)) {vis = true; break;}
+			}
+			if(vis){printf(" %d", b[i].id);}
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
