3d43124f4f2659d2422747bdbf57e658c23e4f1b
Dalei Yang <dalya@microsoft.com>
2016-06-13 15:51:05 +0800

    2016.06.13

--- SourceCode/UVA.1594.cpp
+++ SourceCode/UVA.1594.cpp
@@ -1,60 +1,33 @@
-#include<iostream>
 #include<cstdio>
-#include<map>
-#include<set>
-#include<vector>
 #include<algorithm>
 using namespace std;
 
-typedef vector<int> VC;
+#define FOR(i, a, b) for(int i = a; i < b; i++)
 
-bool isZero(VC & a){
-	for(unsigned int i = 0; i< a.size(); i++){ 
-		if(a[i] != 0) return false; 
-	}
-	return true;
-}
-
-void getKey(VC & a, string & s){
-	char t[10];
-	sprintf(t, "%d", a[0]);
-	s.append(t);
-	for(unsigned int i = 1; i < a.size(); i++){
-		s.append("-");
-		sprintf(t, "%d", a[i]);
-		s.append(t);
-	}
-}
+const int maxn = 15 + 5;
+int a[maxn];
 
 int main(){
-	ios::sync_with_stdio(false);
 #ifdef LOCAL
 	freopen("UVA.1594.in", "r", stdin);
 	freopen("UVA.1594.out", "w", stdout);
 #endif
-	int n, k;
-	scanf("%d", &n);
-	while(n--){
-		scanf("%d", &k);
-		VC a; int t;
-		while(k--) {scanf("%d", &t); a.push_back(t);}
-		unsigned int size = a.size();
-		set<string> s; 
-		for(int j = 0; j < 1000; j++){
-			/*
-			if(isZero(a)) {printf("ZERO\n"); break;}
-			string k;
-			getKey(a, k);
-			if(s.count(k)) {printf("LOOP\n"); break;}
-			else {s.insert(k);}
-			*/
+	int T, n; bool f;
+	scanf("%d", &T);
+	while(T--){
+		scanf("%d", &n);
+		FOR(i, 0, n) scanf("%d", a + i);
+		FOR(i, 0, 1000){
 			int c = a[0];
-			for(unsigned int i = 0; i < size; i++){
-				if(i == size - 1) a[size-1] = abs(a[size-1]-c);
-				else a[i] = abs(a[i]-a[i+1]);
+			FOR(j, 0, n){
+				if(j == n - 1) a[n - 1] = abs(a[n - 1]-c);
+				else a[j] = abs(a[j]-a[j + 1]);
 			}
+			f = true;
+			FOR(j, 0, n) if(a[j]) {f = false; break;}
+			if(f) break; 
 		}
-		if(isZero(a)) printf("ZERO\n");
+		if(f) printf("ZERO\n");
 		else printf("LOOP\n");
 	}
 	return 0;
