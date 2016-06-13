8db3782c24593cb4771791bd4c96149a468aa32a
Dalei Yang <dalya@microsoft.com>
2016-06-13 18:35:48 +0800

    2016.06.13

--- SourceCode/UVA.10935.cpp
+++ SourceCode/UVA.10935.cpp
@@ -1,9 +1,7 @@
 #include<cstdio>
-#include<cstring>
+#include<iostream>
+#include<queue>
 using namespace std;
-
-const int maxn = 50 + 5;
-int a[maxn];
 
 int main(){
 #ifdef LOCAL
@@ -11,32 +9,16 @@ int main(){
 	freopen("UVA.10935.out", "w", stdout);
 #endif
 	int n;
-	while(scanf("%d", &n) == 1 && n){
-		memset(a, 0, sizeof(int) * maxn);
-		if(n == 1){
-			printf("Discarded cards:\nRemaining card: 1\n");
-			continue;
+	while(cin >> n && n){
+		queue<int> q; int kase = 0;
+		for(int i = 1; i <= n; i++) q.push(i);
+		cout << "Discarded cards:";
+		while(q.size()>=2){
+			if(kase++) cout<<",";
+			cout << " " << q.front(); q.pop();
+			q.push(q.front()); q.pop();
 		}
-		printf("Discarded cards: 1");
-		int c = 1, idx = 0; a[0] = 1;
-		while(c < n - 1){
-			int num = 2;
-			while(num){
-				idx = (idx + 1) % n;
-				if(a[idx] == 0) num--;
-			}
-			printf(", %d", idx + 1);
-			a[idx] = 1;
-			c++;
-		}
-		printf("\nRemaining card: ");
-		for(;;){
-			idx = (idx + 1) % n;
-			if(a[idx] == 0) {
-				printf("%d\n", idx + 1);
-				break;
-			}
-		}
+		cout << "\nRemaining card: " << q.front() << endl;
 	}
 	return 0;
 }
