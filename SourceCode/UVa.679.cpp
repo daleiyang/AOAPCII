4ccc32d6bc5e638cb323802412c06819fb3907ac
Dalei Yang <dalya@microsoft.com>
2016-06-28 20:15:54 +0800

    UVa.679 LRJ version.

--- SourceCode/UVa.679.cpp
+++ SourceCode/UVa.679.cpp
@@ -1,30 +1,21 @@
 #include<cstdio>
-#include<cstring>
 using namespace std;
-
-const int maxn = 20;
-int tree[1 << maxn];
 
 int main(){
 #ifdef LOCAL
 	freopen("UVa.679.in", "r", stdin);
 	freopen("UVa.679.out", "w", stdout);
 #endif
-	int T, D, I, k, n;
+	int T, D, I;
 	scanf("%d", &T);
 	while(T--){
-		memset(tree, 0 , sizeof(tree));
 		scanf("%d%d", &D, &I);
-		k = (1<<D) - 1;
-		for(int i = 1; i <= I; i++){
-			n = 1;
-			for(;;){
-				tree[n] = !tree[n];
-				n = tree[n]? 2*n : 2*n+1;
-				if(n > k) break;
-			}
+		int k = 1;
+		for(int i = 0; i < D - 1; i++){
+			if(I%2){k *= 2; I = I/2+1;}
+			else{k = k*2+1; I /=2;}
 		}
-		printf("%d\n", n/2);
+		printf("%d\n", k);
 	}
 	return 0;
 }
