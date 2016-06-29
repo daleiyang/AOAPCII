a1c39f3e9e9e9e6b3b3a94738edcf1651cac083f
Dalei Yang <dalya@microsoft.com>
2016-06-29 16:10:40 +0800

    UVa.122 static array version.

--- SourceCode/UVa.122.cpp
+++ SourceCode/UVa.122.cpp
@@ -1,53 +1,55 @@
 #include<cstdio>
+#include<cstdlib>
 #include<cstring>
 #include<vector>
 #include<queue>
 using namespace std;
 
 const int maxn = 256 + 10;
+const int root = 1;
 
-struct Node{
-	bool have_value;
-	int v;
-	Node * left, * right;
-	Node():have_value(false), left(NULL), right(NULL){}
-};
+int left[maxn], right[maxn], value[maxn], cnt;
+bool have_value[maxn];
 
-Node * root;
-
-void remove_tree(Node * u){
-	if(u == NULL) return;
-	remove_tree(u->left);
-	remove_tree(u->right);
-	delete u;
+void newtree(){
+	memset(left, 0, sizeof(left));
+	memset(right, 0, sizeof(right));
+	memset(have_value, 0, sizeof(have_value));
+  	left[root] = right[root] = 0;
+	have_value[root] = false;
+	cnt = root;
 }
 
-Node * newnode(){return new Node();}
+int newnode(){
+	int u = ++cnt; 
+	left[u] = right[u] = 0; 
+	have_value[u] = false;
+	return u;
+}
 
 bool failed;
 void addnode(int v, char * s){
 	int n = strlen(s);
-	Node * u = root;
+	int u = root;
 	for(int i = 0; i < n; i++){
 		if(s[i] == 'L'){
-			if(u->left == NULL) u->left = newnode();
-			u = u->left;
+			if(left[u] == 0) left[u] = newnode();
+			u = left[u];
 		}
 		else if(s[i] == 'R'){
-			if(u->right == NULL) u->right = newnode();
-			u = u->right;
+			if(right[u] == 0) right[u] = newnode();
+			u = right[u];
 		}
 	}
-	if(u->have_value) failed = true;
-	u->v = v;
-	u->have_value = true;
+	if(have_value[u]) failed = true;  //test repeated assignment
+	value[u] = v;
+	have_value[u] = true;
 }
 
 char s[maxn];
 bool read_input(){
 	failed = false;
-	remove_tree(root);
-	root = newnode();
+	newtree();
 	for(;;){
 		if(scanf("%s", s) != 1) return false;
 		if(!strcmp(s, "()")) break;
@@ -59,15 +61,15 @@ bool read_input(){
 }
 
 bool bfs(vector<int> & ans){
-	queue<Node *> q;
+	queue<int> q;
 	ans.clear();
 	q.push(root);
 	while(!q.empty()){
-		Node * u = q.front(); q.pop();
-		if(!u->have_value) return false;
-		ans.push_back(u->v);
-		if(u->left != NULL) q.push(u->left);
-		if(u->right != NULL) q.push(u->right);
+		int u = q.front(); q.pop();
+		if(!have_value[u]) return false; // test non-assignment node
+		ans.push_back(value[u]);
+		if(left[u] != 0) q.push(left[u]);
+		if(right[u] != 0) q.push(right[u]);
 	}
 	return true;
 }
