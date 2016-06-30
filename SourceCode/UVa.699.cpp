f4827b843615f25ddacaf42cf757f07897c76f82
Dalei Yang <dalya@microsoft.com>
2016-06-30 17:31:15 +0800

    UVa.699

---
+++ SourceCode/UVa.699.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+using namespace std;
+
+const int maxn = 1000 + 5;
+
+int sum[maxn];
+
+void build(int pos){
+	int v; cin >> v;
+	if(v == -1) return;
+	sum[pos] += v;
+	build(pos-1); build(pos+1);
+}
+
+bool init(){
+	memset(sum, 0 ,sizeof(sum));
+	int v;
+	cin >> v;
+	if(v == -1){ return false;}
+	int pos = maxn/2;
+	sum[pos] = v;
+	build(pos - 1); build(pos + 1);
+	return true;
+}
+
+int main(){
+	ios::sync_with_stdio(false);
+#ifdef LOCAL
+	freopen("UVa.699.in", "r", stdin);
+	freopen("UVa.699.out", "w", stdout);
+#endif
+	int kase = 0;
+	while(init()){
+		int p = 0;
+		while(sum[p] == 0 ) p++;
+		cout << "Case " << ++kase << ":\n" << sum[p++];
+		while(sum[p] != 0){cout << " " << sum[p++];}
+		cout << "\n\n";
+	}
+	return 0;
+}
+
