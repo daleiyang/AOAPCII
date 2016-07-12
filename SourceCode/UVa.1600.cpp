91f1bb7a532c2d58746c6fb520fb69c8b506d9b5
Dalei Yang <dalya@microsoft.com>
2016-07-12 14:33:38 +0800

    Uva.160 DFS Version.

--- SourceCode/UVa.1600.cpp
+++ SourceCode/UVa.1600.cpp
@@ -1,39 +1,31 @@
 #include<cstdio>
 #include<cstring>
-#include<queue>
+#include<algorithm>
 using namespace std;
 
-int n, m, k, T;
+int n, m, k, T, ans;
 const int maxn = 20+5;
 int board[maxn][maxn], vis[maxn][maxn][maxn];
 int dx[] = {0,  0, 1, -1};
 int dy[] = {1, -1, 0,  0};
 
-struct C{
-	int x, y, step, layer;
-	C(int x = 1, int y = 1, int step = 0, int layer = 0):x(x), y(y), step(step), layer(layer){}
-};
-
-int ans(){
-	queue<C> q;
-	memset(vis, 0, sizeof(vis));
-	q.push(C(1, 1, 0, 0));
-	vis[1][1][0] = 1;
-	while(!q.empty()){
-		C tc = q.front(); q.pop();
-		if(tc.x == n && tc.y == m) return tc.step;
-		for(int i = 0; i < 4; i++){
-			int x = tc.x + dx[i];
-			int y = tc.y + dy[i];
-			int layer = tc.layer;
-			if(board[x][y]) layer++; else layer = 0;
-			if(!vis[x][y][layer] && layer <= k && x > 0 && x <= n && y > 0 && y <= m){
-				vis[x][y][layer] = 1;
-				q.push(C(x, y, tc.step+1, layer));
-			}
+void dfs(int x, int y, int step, int layer){
+	if(x == n && y == m){
+		ans = min(ans, step);
+		return;
+	}
+	for(int i = 0; i < 4; i++){
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+		int nlayer = layer;
+		if(board[nx][ny] == 1) nlayer++; else nlayer = 0;
+		if((vis[nx][ny][nlayer] < 0 || vis[nx][ny][nlayer] > step+1)
+			&& nlayer <= k && nx >= 1 && nx <= n && ny >= 1 && ny <= m){
+			vis[nx][ny][nlayer] = step + 1;
+			dfs(nx, ny, step+1, nlayer);
 		}
 	}
-	return -1;
+	return;
 }
 
 int main(){
@@ -49,7 +41,11 @@ int main(){
 				scanf("%d", &board[i][j]);
 			}
 		}
-		printf("%d\n", ans());
+		memset(vis, -1 ,sizeof(vis));
+		ans = 1 << 30;
+		dfs(1, 1, 0, 0);
+		if(ans == 1<<30) printf("-1\n");
+		else printf("%d\n", ans);
 	}
 	return 0;
 }
