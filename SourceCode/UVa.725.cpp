c2a0a0446719118b1c121d4bf1e6b77f0dfdbd00
Dalei Yang <dalya@microsoft.com>
2016-07-21 23:10:08 +0800

    UVa.725 version 2.

--- SourceCode/UVa.725.cpp
+++ SourceCode/UVa.725.cpp
@@ -3,36 +3,38 @@
 using namespace std;
 
 const int S = 1234, E = 98765;
-int f[10], y;
-
-bool validate(int z){
-	if(z > 99999) return false;
-	if(z < 10000 && f[0]) return false;
-	else if(z < 10000 && !f[0]) {f[0] = 1;}
-	do{
-		y = z % 10, z = z / 10;
-		if(f[y]) return false;
-		else f[y] = 1;
-	}while(z);
-	return true;
-}
+int f[10];
 
 int main(){
 #ifdef LOCAL
 	freopen("UVa.725.in", "r", stdin);
 	freopen("UVa.725.out", "w", stdout);
 #endif
-	int n,kase = 0;
+	int n, kase = 0;
 	while(scanf("%d", &n) == 1 && n){
 		if(kase++) {printf("\n");}
 		bool flag = false;
-		 for(int i = S; i <= E; i++){
+		int a = S, b = a*n;
+		for(; b <= E; a++, b=a*n){
 			memset(f, 0, sizeof(f));
-			if(!validate(i)) continue;
-			int j = i * n;
-			if(!validate(j)) continue;
-			printf("%05d / %05d = %d\n", j, i, n);
-			flag = true;
+			if(a < 10000 || b < 10000) f[0] = 1;
+			int i = a, j = b;
+			while(i){
+				f[i%10]++;
+				i /= 10;
+			}
+			while(j){
+				f[j%10]++;
+				j /= 10;
+			}
+			int k = 0;
+			for(int i = 0; i < 10; i++){
+				if(f[i] == 1) k++;
+			}
+			if(k == 10){
+				printf("%05d / %05d = %d\n", b, a, n);
+				flag = true;
+			}
 		 }
 		 if(!flag) {
 		 	printf("There are no solutions for %d.\n", n);
