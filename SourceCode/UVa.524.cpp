a8148c8d289a84801c411d3f0bba8a466f15fc81
Dalei Yang <dalya@microsoft.com>
2016-07-25 23:35:44 +0800

    Uva.524 backtracking version.

--- SourceCode/UVa.524.cpp
+++ SourceCode/UVa.524.cpp
@@ -1,16 +1,35 @@
 #include<cstdio>
+#include<cstring>
 #include<algorithm>
 using namespace std;
 
-const int maxn = 16;
-int isp[maxn*2];
-int A[maxn];
+const int maxn = 25;
+int isp[maxn*2], A[maxn], vis[maxn], n;
 
 int is_prime(int x){
 	for(int i = 2; i*i <= x; i++){
 		if(x % i == 0) return 0;
 	}
 	return 1;
+}
+
+void dfs(int cur){
+	if(cur == n && isp[A[n-1]+A[0]]){
+		for(int i = 0; i < n; i++) {
+			if(i == 0) printf("%d", A[i]);
+			else printf(" %d", A[i]);
+		}
+		printf("\n");
+	}else{
+		for(int i = 2; i <= n; i++){
+			if(!vis[i] && isp[A[cur-1]+i]){
+				A[cur] = i;
+				vis[i] = 1;
+				dfs(cur+1);
+				vis[i] = 0;
+			}
+		}
+	}
 }
 
 int main(){
@@ -21,28 +40,13 @@ int main(){
 	for(int i = 2; i < maxn*2; i++){
 		isp[i] = is_prime(i);
 	}
-	int n, kase = 0;
+	int kase = 0;
 	while(scanf("%d", &n) == 1){
+		if(kase) printf("\n");
+		memset(vis, 0, sizeof(vis));
 		printf("Case %d:\n", ++kase);
-		for(int i = 0; i < n; i++){
-			A[i] = i+1;
-		}
-		do{
-			int ok = 1;
-			for(int i = 0; i < n; i++){
-				if(!isp[A[i]+A[(i+1)%n]]){
-					ok = 0; break;
-				}
-			}
-			if(ok){
-				for(int i = 0; i < n; i++){
-					printf("%d ", A[i]);
-				}
-				printf("\n");
-			}
-
-		}while(next_permutation(A+1, A+n));
-		printf("\n");
+		A[0] = 1;
+		dfs(1);
 	}
 	return 0;
 }
