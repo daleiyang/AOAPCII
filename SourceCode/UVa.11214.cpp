bb9a25e27837761d897d2d138e1499cd53fde1e0
Dalei Yang <dalya@microsoft.com>
2016-08-30 17:48:54 +0800

    UVa.11214 correct version

--- SourceCode/UVa.11214.cpp
+++ SourceCode/UVa.11214.cpp
@@ -4,23 +4,26 @@ using namespace std;
 
 const int maxn = 10 + 5;
 
-int r, c, vis[4][20], maxd;
+int r, c, vis[4][20], maxd, vis2[maxn][maxn];
 char mp[maxn][maxn];
 
-bool dfs(int row, int d){
+bool dfs(int d){
 	if(d == maxd){
 		for(int i = 0; i < r; i++) for(int j = 0; j < c; j++){
-			if(mp[i][j] == 'X' && !vis[0][i] && !vis[1][j] && !vis[2][i-j+11] && !vis[3][i+j]){
+			if(mp[i][j] == 'X' && !vis[0][i] && !vis[1][j] && !vis[2][i-j+10] && !vis[3][i+j]){
 				return false;
 			}
 		}
 		return true;
 	}
-	for(int i = row; i < r; i++) for(int j = 0; j < c; j++){
-		int t1 = vis[0][i], t2 = vis[1][j], t3 = vis[2][i-j+11], t4 = vis[3][i+j];
-		vis[0][i] = vis[1][j] = vis[2][i-j+11] = vis[3][i+j] = 1;
-		if(dfs(i+1, d+1)) return true;
-		vis[0][i] = t1; vis[1][j] = t2; vis[2][i-j+11] = t3; vis[3][i+j] = t4;
+	for(int i = 0; i < r; i++) for(int j = 0; j < c; j++){
+		if(vis2[i][j]) continue;
+		vis2[i][j] = 1;
+		int t1 = vis[0][i], t2 = vis[1][j], t3 = vis[2][i-j+10], t4 = vis[3][i+j];
+		vis[0][i] = vis[1][j] = vis[2][i-j+10] = vis[3][i+j] = 1;
+		if(dfs(d+1)) return true;
+		vis[0][i] = t1; vis[1][j] = t2; vis[2][i-j+10] = t3; vis[3][i+j] = t4;
+		vis2[i][j] = 0;
 	}
 	return false;
 }
@@ -36,7 +39,8 @@ int main(){
 		for(int i = 0; i < r; i++){scanf("%s", mp[i]);}
 		for(maxd = 0; maxd < 10; maxd++){
 			memset(vis, 0, sizeof(vis));
-			if(dfs(0, 0)) break;
+			memset(vis2, 0, sizeof(vis2));
+			if(dfs(0)) break;
 		}
 		printf("Case %d: %d\n", ++kase, maxd);
 	}
