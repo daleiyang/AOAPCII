93516cffc5d8ccb6f89b63dc2628c26da0aae9e9
REDMOND\dalya <dalya@microsoft.com>
2016-09-13 12:59:29 +0800

    UVa.11846 fast version revisit.

--- SourceCode/UVa.11846.cpp
+++ SourceCode/UVa.11846.cpp
@@ -1,56 +1,48 @@
 #include<cstdio>
 #include<cctype>
+using namespace std;
 
 #define REP(i, b, n) for(int i = b; i < n; i++)
 #define rep(i, n) REP(i, 0, n)
 
-using namespace std;
+const int M = 20;
 
-const int N = 20;
-
-char t[N][N], ans[N][N];
+char t[M][M], ans[M][M];
 int n;
 
-bool dfs(char c, int pos){
-  while(pos != n * n && ans[pos/n][pos%n] != '.')pos++;
-  if(pos == n*n)return true;
-  int bx = pos%n, by = pos/n;
-  REP(h, by, n){
-    REP(w, bx, n){
-      int cnt = 0, area = (h - by + 1) * (w - bx + 1);
-      if( area > 9)break;
-      if(ans[h][w] != '.')break;
-      int now = -1;
-      bool out = false;
-      REP(y, by, h+1){
-        REP(x, bx, w+1){
-          if(isdigit(t[y][x])){
-            cnt++;
-            now = (t[y][x] - '0');
-          }
-          if(ans[y][x] != '.'){
-            out = true;
-            break;
-          }
-        }
-        if(out || cnt > 1 || (now != -1 && now != area))break;
-      }
-      if(cnt > 1 || (now != -1 && (area > now || now % (h-by+1) != 0 ) ) || out)break;
-      if(cnt == 0 || now > area)continue;
-      REP(y, by, h+1){
-        REP(x, bx, w+1){
-          ans[y][x] = c;
-        }
-      }
-      if(dfs(c+1, pos+(w-bx+1)))return true;
-      REP(y, by, h+1){
-        REP(x, bx, w+1){
-          ans[y][x] = '.';
-        }
-      }
-    }
-  }
-  return false;
+bool dfs(char c, int d){
+	while(d != n*n && ans[d/n][d%n] != '.') d++;
+	if(d == n*n) return true;
+	int dx = d%n, dy = d/n;
+	REP(h, dy, n) REP(w, dx, n){
+		int area = (h-dy+1)*(w-dx+1);
+		if(area > 9) break;
+		if(ans[h][w] != '.') break;
+		int now  = -1, out = false, cnt = 0;
+		REP(y, dy, h+1){
+			REP(x, dx, w+1){
+				if(isdigit(t[y][x])){
+					cnt++;
+					now = t[y][x] - '0';
+				}
+				if(ans[y][x] != '.'){
+					out = true;
+					break;
+				}
+			}
+			if(cnt > 1 || out || (now != -1 && now != area)) break;
+		}
+		if(cnt > 1 || out || (now != -1 && (area > now || now%(h-dy+1) != 0))) break;
+		if(cnt == 0 || now > area) continue;
+		REP(y, dy, h+1) REP(x, dx, w+1){
+			ans[y][x] = c;
+		}
+		if(dfs(c+1, d+(w-dx+1))) return true;
+		REP(y, dy, h+1) REP(x, dx, w+1){
+			ans[y][x] = '.';
+		}
+	}
+	return false;
 }
 
 int main(){
@@ -58,23 +50,18 @@ int main(){
 	freopen("UVa.11846.in", "r", stdin);
 	freopen("UVa.11846.out", "w", stdout);
 #endif
-  int tmp ;
-  while(scanf("%d%d", &n, &tmp)){
-    if( n == 0 && tmp == 0)break;
-    rep(i, n){
-      rep(j, n){
-        scanf(" %c ",&t[i][j]);
-        ans[i][j] = '.';
-      }
-    }
-    dfs('A', 0);
-    rep(i, n){
-      rep(j, n){
-        putchar(ans[i][j]);
-      }
-      putchar('\n');
-    }
-  }
-  return 0;
+	int temp;
+	while(scanf("%d%d", &n, &temp) == 2 && n){
+		rep(i, n) rep(j, n){
+			scanf(" %c ", &t[i][j]); //interesting, with two black space, we could swallow the /r/n
+			ans[i][j] = '.';
+		}
+		dfs('A', 0);
+		rep(i, n){
+			rep(j, n) printf("%c", ans[i][j]);
+			printf("\n");
+		}
+	}
+	return 0;
 }
 
