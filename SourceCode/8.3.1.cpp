f02af95c73e05bc4d5eb8709b03d8c85d55d3345
REDMOND\dalya <dalya@microsoft.com>
2016-09-23 16:10:03 +0800

    8.3.1

--- SourceCode/8.3.1.cpp
+++ SourceCode/8.3.1.cpp
@@ -1,58 +1,50 @@
-#include <cstring>
-#include <cstdio>
-#include <cstdlib>
+#include<cstdio>
+#include<cstring>
 using namespace std;
-const int MAX = 32;
-int n;
-int A[MAX][MAX];
-int st;
 
-void print()
-{
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            printf("%3d", A[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
+const int maxn = 64;
+
+int G[maxn][maxn], n, st;
+
+void print(){
+	for(int r = 0; r < n; r++){
+		for(int c = 0; c < n; c++) printf("%3d", G[r][c]);
+		printf("\n");
+	}
+	printf("\n");
 }
 
-void dfs(int x, int y, int w)
-{
-    if(w == 1) {
-        return;
-    }
-    int i, j;
-    int found = 0;
-    for(i=x; i<x+w; i++) {
-        for(j=y; j<y+w; j++) {
-            if(A[i][j] != 0) {
-                found = 1;
-                break;
-            }
-        }
-        if(found) break;
-    }
-    int mx = x+w/2, my = y+w/2;
-    int bx = i < mx;
-    int by = j < my;
-    if(bx) {
-        A[mx][my-1] = A[mx][my] = st;
-        if(by) A[mx-1][my] = st;
-        else A[mx-1][my-1] = st;
-    } else {
-        A[mx-1][my] = A[mx-1][my-1] = st;
-        if(by) A[mx][my] = st;
-        else A[mx][my-1] = st;
-    }
-    print();
-    st++;
-    int tw = w/2;
-    dfs(mx-tw, my-tw, tw);
-    dfs(mx-tw, my, tw);
-    dfs(mx, my-tw, tw);
-    dfs(mx, my, tw);
+void dfs(int r, int c, int w){
+	if(w == 1) return;
+	int i, j;
+	bool f = false;
+	for(i = r; i < r + w; i++){
+		for(j = c; j < c + w; j++){
+			if(G[i][j] != 0) {
+				f = true;
+				break;
+			}
+		}
+		if(f) break;
+	}
+	int dr = r + w/2;
+	int dc = c + w/2;
+	if(i < dr){
+		G[dr][dc] = G[dr][dc-1] = st;
+		if(j < dc) G[dr-1][dc] = st;
+		else G[dr-1][dc-1] = st;
+	} else{
+		G[dr-1][dc] = G[dr-1][dc-1] = st;
+		if(j < dc) G[dr][dc] = st;
+		else G[dr][dc-1] = st;
+	}
+	print();
+	st++;
+	int nw = w/2;
+	dfs(r, c, nw);
+	dfs(r, c+nw, nw);
+	dfs(r+nw, c, nw);
+	dfs(r+nw, c+nw, nw);
 }
 
 int main(){
@@ -60,17 +52,16 @@ int main(){
 	freopen("8.3.1.in", "r", stdin);
 	freopen("8.3.1.out", "w", stdout);
 #endif
-    int k;
-    int gx, gy;
-    while(scanf("%d%d%d", &k, &gx, &gy) == 3) {
-        memset(A, 0, sizeof(A));
-        st = 1;
-        n = 1 << k;
-        A[gx][gy] = -1;
-        print();
-        dfs(0, 0, n);
-        print();
-    }
-    return 0;
+	int r, c, k;
+	while(scanf("%d%d%d", &k, &r, &c) == 3 && k){
+		n = 1<<k;
+		memset(G, 0, sizeof(G));
+		G[r][c] = -1;
+		st = 1;
+		print();
+		dfs(0, 0, n);
+		print();
+	}
+	return 0;
 }
 
