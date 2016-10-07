fe2df4d60e3996c99ddb09acaeac19a1b81b877a
REDMOND\dalya <dalya@microsoft.com>
2016-10-07 16:23:03 +0800

    UVa.1451 no STL verison.

--- SourceCode/UVa.1451.cpp
+++ SourceCode/UVa.1451.cpp
@@ -1,12 +1,10 @@
 #include<cstdio>
-#include<queue>
 using namespace std;
 
 const int maxn = 100000 + 5;
 
 char s[maxn];
-int sum[maxn];
-deque<int> q;
+int sum[maxn], p[maxn];
 
 int compare_avg(int x1, int x2, int x3, int x4){
 	return (sum[x2]-sum[x1-1])*(x4-(x3-1)) - (sum[x4]-sum[x3-1])*(x2-(x1-1));
@@ -25,28 +23,17 @@ int main(){
 		
 		sum[0] = 0;
 		for(int i = 1; i <= n; i++) sum[i] = sum[i-1]+s[i]-'0';
-		
-		q.clear();
+
 		int ansL = 1, ansR = L;
+		int i = 0, j = 0;
 		for(int t = L; t <= n; t++){
-			while(q.size()>1){
-				int b = q[q.size()-1];
-				int b2 = q[q.size()-2];
-				if(compare_avg(b, t-L, b2, t-L) < 0){
-					q.pop_back();
-				}
-				else break;
-			}
-			q.push_back(t-L+1);
-			while(q.size()>1){
-				if(compare_avg(q[0], t, q[1], t) <= 0){
-					q.pop_front();
-				}
-				else break;
-			}
-			int c = compare_avg(q[0], t, ansL, ansR);
-			if(c > 0 || (c == 0 && t-q[0] < ansR- ansL)){
-				ansL = q[0];
+			while(j-i > 1 && compare_avg(p[j-2], t-L, p[j-1], t-L) >= 0) j--;
+			p[j++] = t-L+1;
+			while(j-i > 1 && compare_avg(p[i], t, p[i+1], t) <= 0) i++;
+				
+			int c = compare_avg(p[i], t, ansL, ansR);
+			if(c > 0 || (c == 0 && t-p[i] < ansR- ansL)){
+				ansL = p[i];
 				ansR = t;
 			}
 		}
