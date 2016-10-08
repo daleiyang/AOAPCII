ee0506efe54ea7aaff543f1cce0896b38abdf6ce
REDMOND\dalya <dalya@microsoft.com>
2016-10-09 02:45:02 +0800

    UVa.1607

---
+++ SourceCode/UVa.1607.cpp
@@ -0,0 +1,60 @@
+#include<cstdio>
+using namespace std;
+
+const int maxn = 100000 + 5;
+const int maxm = 200000 + 5;
+
+int n, m;
+
+struct Gates{
+	int a, b, o;
+}gates[maxm];
+
+int output(int k){
+	for(int i = 1; i <= m; i++){
+		int a = gates[i].a;
+		int b = gates[i].b;
+		int va = a < 0 ? -a > k : gates[a].o;
+		int vb = b < 0 ? -b > k : gates[b].o;
+		gates[i].o = !(va && vb);
+	}
+	return gates[m].o;
+}
+
+int solve(int vn){
+	int L = 1, R = n;
+	while(L < R){
+		int M = L + (R-L)/2;
+		if(output(M) == vn) R = M; else L = M+1;
+	}
+	return L;
+}
+
+int main(){
+#ifdef LOCAL
+	freopen("UVa.1607.in", "r", stdin);
+	freopen("UVa.1607.out", "w", stdout);
+#endif
+	int T;
+	scanf("%d", &T);
+	while(T--){
+		scanf("%d%d", &n, &m);
+		for(int i = 1; i <= m; i++){
+			scanf("%d%d", &gates[i].a, &gates[i].b);
+		}
+		int v0 = output(0);
+		int vn = output(n);
+		if(v0 == vn){
+			for(int i = 1; i <= n; i++) printf("0");
+		}
+		else{
+			int x = solve(vn);
+			for(int i = 1; i < x; i++) printf("0");
+			printf("x");
+			for(int i = x+1; i <= n; i++) printf("1");
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
