fd6c2895153737bad5403b249e0b6e88569f7296
REDMOND\dalya <dalya@microsoft.com>
2016-10-08 15:55:44 +0800

    UVa.12627 refined version.

--- SourceCode/UVa.12627.cpp
+++ SourceCode/UVa.12627.cpp
@@ -1,47 +1,45 @@
 #include<cstdio>
+#include<iostream>
 using namespace std;
 
 typedef long long LL;
 int K;
 
 LL c(int k){
-	LL ans = 1;
-	for(int i = 0; i < k; i++) ans *= 3;
-	return ans;	
+	return k == 0 ? 1 : c(k-1)*3;
 }
 
 LL f(int k, int i){
-	if(k == 0 && i == 1) return 1;
-	else if(i < 1) return 0;
-	else if(i <= (1 << (k-1))) return 2 * f(k-1, i);
-	else if(i > (1 << (k-1))) return 2 * c(k-1) + f(k-1, i - (1 << (k-1)));
-	return 0;
+	if(i == 0) return 0;
+	if(k == 0) return 1;
+
+	int k2 = 1 << (k-1);
+	if(i <= k2) return 2 * f(k-1, i);
+	else return 2 * c(k-1) + f(k-1, i-k2);
 }
 
 LL g(int k, int i){
-	if(k == 0 && i == 1) return 1;
-	else if(i > (1 << K)) return 0;
-	else if(i <= (1 << (k-1))) return c(k-1) + 2* g(k-1, i);
-	else if (i > (1 << (k-1))) return g(k-1, i - (1 << (k-1)));
-	return 0;
-}
+	if(i > (1 << K)) return 0;
+	if(k == 0) return 1;
 
-LL solve(int k, int a, int b){	
-	return f(k, b) - f(k, a-1);
-	//return g(k, a) - g(k, b+1);
+	int k2 = 1 << (k-1);
+	if(i <= k2) return c(k-1) + 2* g(k-1, i);
+	else return g(k-1, i-k2);
 }
 
 int main(){
+	ios::sync_with_stdio(false);
 #ifdef LOCAL
 	freopen("UVa.12627.in", "r", stdin);
 	freopen("UVa.12627.out", "w", stdout);
 #endif
 	int T;
-	scanf("%d", &T);
+	cin >> T;
 	for(int i = 1; i <= T; i++){
 		int A, B;
-		scanf("%d%d%d", &K, &A, &B);
-		printf("Case %d: %lld\n", i, solve(K, A, B));
+		cin >> K >> A >> B;
+		//cout << "Case " << i << ": " << f(K, B) - f(K, A-1) << "\n";
+		cout << "Case " << i << ": " <<  g(K, A) - g(K, B+1) << "\n";
 	}
 	return 0;
 }
