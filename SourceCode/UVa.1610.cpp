23ba164d89fe1a469811d1b6986338b014bec6a6
REDMOND\dalya <dalya@microsoft.com>
2016-10-16 17:16:31 +0800

    UVa.1610 test case first, logic second version.

--- SourceCode/UVa.1610.cpp
+++ SourceCode/UVa.1610.cpp
@@ -4,9 +4,7 @@
 #include<algorithm>
 using namespace std;
 
-const int maxL = 30 + 5;
 const int maxN = 1000 + 5;
-char ans[maxL];
 string x[maxN];
 
 int main(){
@@ -19,16 +17,36 @@ int main(){
 	while(cin >> n && n){
 		for(int i = 0; i < n; i++) cin >> x[i];
 		sort(x, x+n);
-		string s = x[(n>>1)-1], t = x[n>>1];
-		int len = s.length(), i = 0;
+		string a = x[(n/2)-1], b = x[n/2];
 		string ans = "";
-		ans += "A";
-		while(i < len){
-			while(ans[i] < 'Z' && ans < s) ans[i]++;
-			if(ans >= s && ans < t) break;
-			ans[i] = s[i];
-			ans += "A";
-			i++;
+		for(unsigned int i = 0; i < a.size() && i < b.size(); i++){
+			if(a[i] == b[i]){
+				ans += a[i];
+			}
+			else{
+				if(i == a.size()-1){
+					ans += a[i];
+					break;
+				}
+				else if(b[i] - a[i] > 1 || i != b.size()-1){
+					ans += a[i]+1;
+					break;
+				}else{
+					ans += a[i];
+					for(unsigned int j = i+1; j < a.size(); j++){
+						if(j == a.size()-1){
+							ans += a[j];
+							break;
+						}
+						else if(a[j] != 'Z'){
+							ans += a[j]+1;
+							break;
+						}
+						ans += 'Z';
+					}
+					break;
+				}
+			}
 		}
 		cout << ans << endl;
 	}
