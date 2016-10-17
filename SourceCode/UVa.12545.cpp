2b841ce0763482fd4febbfd93dbf34c93485f6b0
REDMOND\dalya <dalya@microsoft.com>
2016-10-17 10:29:43 +0800

    UVa.12545 scanf once version.

--- SourceCode/UVa.12545.cpp
+++ SourceCode/UVa.12545.cpp
@@ -1,5 +1,6 @@
 #include<cstdio>
 #include<cstring>
+#include<algorithm>
 using namespace std;
 
 const int maxn = 100 + 5;
@@ -10,56 +11,40 @@ int main(){
 		freopen("UVa.12545.in", "r", stdin);
 		freopen("UVa.12545.out", "w", stdout);
 #endif
-	int n, len;
+	int n;
 	scanf("%d", &n);
 	for(int t = 1; t <= n; t++){
-		scanf("%s", a);
-		scanf("%s", b);
-		len = strlen(a);
-		int a0n = 0, aqn = 0, b0n = 0;
-		for(int i = 0; i < len; i++){
-			if(a[i] == '0') a0n++;
-			if(a[i] == '?') aqn++;
-			if(b[i] == '0') b0n++;
+		scanf("%s%s", a, b);
+		int n01 = 0, n10 = 0, nq1 = 0, nq0 = 0;
+		for(unsigned int i = 0; i < strlen(a); i++){
+			if(a[i] == '0' && b[i] == '1') n01++;
+			if(a[i] == '1' && b[i] == '0') n10++;
+			if(a[i] == '?' && b[i] == '1') nq1++;
+			if(a[i] == '?' && b[i] == '0') nq0++;
 		}
-		if(a0n + aqn < b0n) {
+		if(n01+nq1+nq0 < n10+nq0) {
 			printf("Case %d: -1\n", t);
 			continue;
 		}
 		int ans = 0;
-		if(a0n < b0n){
-			for(int i = 0; a0n < b0n && i < len; i++){
-				if(a[i] == '?' && b[i] == '0') {
-					a[i] = '0'; a0n++; ans++;
-				}
-			}
-			for(int i = 0; a0n < b0n && i < len; i++){
-				if(a[i] == '?') {
-					a[i] = '0'; a0n++; ans++;
-				}
-			}
+		if(n01 < n10+nq0){
+			int d = n10+nq0-n01;
+			int t = min(d, nq0);
+			nq0 -= t;
+			ans += t;
+			if(d > t){nq1 -= d-t; n01 += d-t; ans += d-t;}
 		}
-
-		if(a0n > b0n){
-			for(int i = 0; a0n > b0n && i < len; i++){
-				if(a[i] == '0' && b[i] == '1') {
-					a[i] = '1'; a0n--; ans++;
-				}
-			}
+		if(n01 > n10+nq0){
+			int d = n01-(n10+nq0); 
+			n01 -= d; 
+			ans += d;
 		}
-
-		for(int i = 0; i < len; i++){
-				if(a[i] == '?') {
-					a[i] = '1'; ans++;
-				}
+		if(nq1 > 0) ans += nq1;
+		if(nq0 > 0) {
+			ans += nq0;
+			n10 += nq0;
 		}
-
-		int cnt = 0;
-		for(int i = 0; i < len; i++){
-			if(a[i] != b[i]) cnt++;
-		}
-
-		ans += cnt/2;
+		ans += (n10+ n01)/2;
 		printf("Case %d: %d\n", t, ans);
 	}
 	return 0;
