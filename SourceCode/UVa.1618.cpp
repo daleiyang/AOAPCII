e14396349d5fcefa06407f6b65a0c70a8b7a285d
REDMOND\dalya <dalya@microsoft.com>
2016-11-06 00:12:30 +0800

    UVa.1618 final version with algorithm comments.

--- SourceCode/UVa.1618.cpp
+++ SourceCode/UVa.1618.cpp
@@ -1,125 +1,81 @@
-//http://blog.csdn.net/playwfun/article/details/51125144
-#include <cstdio>
-#include <algorithm>
-#include <cstring>
-#include <iostream>
-#include <map>
-#include <vector>
+//http://blog.csdn.net/zyq522376829/article/details/46649767
+#include<cstdio>
+#include<algorithm>
 using namespace std;
 
-typedef long long ll;
-#define rep( i , n) for(int i = 0 ; i<(int)n;i++)
-#define rep1(i ,x , y) for(int i = (int)x ; i<=(int)y;i++)
-const int N = 5050;
+const int maxn = 5000 + 5;
 
-int max_[N][20],min_[N][20],lg2[N]; //
-void ST(int *a,int n)
-{
-    lg2[0]=-1;
-    for(int i=1; i<=n; i++)
-        lg2[i]=lg2[i-1]+(i&(i-1)?0:1);
-    for(int i=0; i<n; i++) max_[i][0]=a[i]; //a
-    for(int j=1; j<=lg2[n]; j++)
-        for(int i=0; lg2[n-i]>=j; i++)
-            max_[i][j]=max(max_[i][j-1],max_[i+(1<<(j-1))][j-1]);
-}
-int RMQ_Max(int x,int y)
-{
-    int k=lg2[y-x+1];
-    return max(max_[x][k],max_[y-(1<<k)+1][k]);
-}
-void ST2(int *a,int n)
-{
-    lg2[0]=-1;
-    for(int i=1; i<=n; i++)
-        lg2[i]=lg2[i-1]+(i&(i-1)?0:1);
-    for(int i=0; i<n; i++) min_[i][0]=a[i]; //a
-    for(int j=1; j<=lg2[n]; j++)
-        for(int i=0; lg2[n-i]>=j; i++)
-            min_[i][j]=min(min_[i][j-1],min_[i+(1<<(j-1))][j-1]);
-}
-int RMQ_Min(int x,int y)
-{
-    int k=lg2[y-x+1];
-    return min(min_[x][k],min_[y-(1<<k)+1][k]);
+int num[maxn], L[maxn][maxn], R[maxn][maxn], n;
+/*
+We have 1 <= p < q < r < s <= n  &&  Nq > Ns > Np > Nr.
+
+We enumerate all possible q < r to minimize time complexity.
+
+For every q < r  and Nq > Nr, we need to identify one "p" letting p < q < r and Np > Nr.  
+In order to find a probable solution, we need find a Np which is the closest one to Nr. Make more room for Ns.
+Array "L" is for this purpose.
+
+For every q < r and Nq > Nr, we need to identify one "s" and q < r < s and Nq > Ns. 
+In order to find a probable solution, we need find a Ns which is the closest one to Nq. Make more room for Np.
+Array "R" is for this purpose.
+
+After we identfy "p" and "s", we purning based on 1 <= p and s <= n. So 1 <= p < q < r < s <= n is meet.
+we pruning based on Nq > Ns  and Ns > Np and Np > Nr. So Nq > Ns > Np > Nr. is meet.
+
+For Nq < Ns < Np < Nr case, we only need to reverse the array "num" and apply the same logic.
+Just write it down with sample data, we could make sure the logic is correct.
+*/
+bool solve(){
+	//find a value which is larger than Ni and it's the smallest larger value than Ni among all Nj where j < i.
+	//take this sample to understand: 10 30 60 40 20 50
+	for(int i = 1; i <= n; i++){
+		L[i][0] = 0; //init value for following steps
+		for(int j = 1; j < i; j++){
+			if(num[i] >= num[j]) L[i][j] = L[i][j-1];
+			else if(!L[i][j-1] || num[j] < num[L[i][j-1]]) L[i][j] = j;
+			else L[i][j] = L[i][j-1];
+		}
+	}
+
+	//find a value which is smaller than Ni and it's the largest smaller value than Ni among all Nj where j > i.
+	for(int i = 1; i <= n; i++){
+		R[i][n+1] = 0; //init value for following steps
+		for(int j = n; j > i; j--){
+			if(num[i] <= num[j]) R[i][j] = R[i][j+1];
+			else if(!R[i][j+1] || num[j] > num[R[i][j+1]] ) R[i][j] = j;
+			else R[i][j] = R[i][j+1];
+		}
+	}
+
+	//Enumerate all possible q < r.
+	for(int q = 1; q <= n; q++){
+		for(int r = q+1; r <= n; r++){
+			//pruning
+			if(num[q] <= num[r] || !R[q][r+1] || !L[r][q-1]) continue;
+			int p = L[r][q-1], s = R[q][r+1];
+			if(num[s] > num[p]) return true;
+		}
+	}
+	return false;
 }
 
-struct San :vector<int>
-{
-    void prepare()
-    {
-        sort(begin(),end());
-        erase(unique(begin(),end()),end());
-    }
-    int get(int x)
-    {
-        return (int)(lower_bound(begin(),end(),x)-begin())+1;
-    }
-} rank2;
-int n,a[N],pos[N];
-bool cal()
-{
-    ST2(a , n);
-    ST(pos , n);
-
-    int ok = 0;
-    for(int i = 1 ; i<=n ; i++)
-    {
-        for(int j = i + 1 ; j<=n ; j++) if(a[j] > a[i])
-            {
-                int L = a[i] + 1;
-                int R = a[j] - 1;
-                if(L > R) continue;
-                int max_pos = RMQ_Max(L , R);
-                R = (--max_pos);
-                L = j + 1;
-                if(L > R) continue;
-                int min_val = RMQ_Min(L , R);
-                if(min_val < a[i]){
-                    ok = 1;
-                    break;
-                }
-            }
-        if(ok) break;
-    }
-    return ok;
-}
-int main()
-{
+int main(){
 #ifdef LOCAL
-			freopen("UVa.1618.in", "r", stdin);
-			freopen("UVa.1618.out", "w", stdout);
+		freopen("UVa.1618.in", "r", stdin);
+		freopen("UVa.1618.out", "w", stdout);
 #endif
-    int T;
-    scanf("%d",&T);
-    while(T--)
-    {
-        scanf("%d",&n);
-        rank2.clear();
-        rep1(i , 1 , n) scanf("%d",&a[i]),rank2.push_back(a[i]);
-        rank2.prepare();
-        for(int i = 1 ; i<=n ; i++)
-        {
-            a[i] = rank2.get(a[i]);
-            pos[a[i]] = i;
-        }
-        if(cal())
-        {
-            printf("YES\n");
-            continue;
-        }
-        for(int i = 1 ; i<=n ; i++)
-        {
-            a[i] = n + 1 - a[i];
-            pos[a[i]] = i;
-        }
-        if(cal())
-        {
-            printf("YES\n");
-            continue;
-        }
-        printf("%s\n","NO");
-    }
-    return 0;
+	int T;
+	scanf("%d", &T);
+	while(T--){
+		scanf("%d", &n);
+		for(int i = 1; i <= n; i++) scanf("%d", &num[i]);
+		if(solve()) printf("YES\n");
+		else{
+			reverse(num+1, num+n+1);
+			if(solve()) printf("YES\n");
+			else printf("NO\n");
+		}
+	}
+	return 0;
 }
 
