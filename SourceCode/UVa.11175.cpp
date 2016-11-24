104411f1699867f284f8663d2ada60c67961bf20
REDMOND\dalya <dalya@microsoft.com>
2016-11-25 01:17:15 +0800

    UVa.11175 without STL version.

--- SourceCode/UVa.11175.cpp
+++ SourceCode/UVa.11175.cpp
@@ -1,30 +1,25 @@
 //http://www.cnblogs.com/137033036-wjl/p/4945084.html
+//https://github.com/morris821028/UVa/blob/master/volume111/11175%20-%20From%20D%20to%20E%20and%20Back.cpp
 #include<cstdio>
-#include<vector>
-#include<set>
+#include<cstring>
 using namespace std;
 
 const int maxn = 300+5;
 
-int m, k;
-vector<int> in[maxn], out[maxn];
-set<int> s;
+int g[maxn][maxn], gt[maxn];
+int invg[maxn][maxn], invgt[maxn], to[maxn];
 
-bool solve(){
+bool solve(int m){
 	for(int i = 0; i < m; i++){
-		if(!in[i].size()) continue;
-		s.clear();
-		int len = out[in[i][0]].size();
-		for(int j = 0; j < (int)out[in[i][0]].size(); j++)
-			s.insert(out[in[i][0]][j]);
-		for(int j = 1; j < (int)in[i].size(); j++){
-			if((int)out[in[i][j]].size() != len){
+		memset(to, 0, sizeof(to));
+		for(int j = 0; j < invgt[i]; j++){
+			for(int k = 0; k < gt[invg[i][j]]; k++){
+				to[g[invg[i][j]][k]]++;
+			}
+		}
+		for(int j = 0; j < m; j++){
+			if(to[j] != 0 && to[j] != invgt[i])
 				return false;
-			}
-			for(int p = 0; p < (int)out[in[i][j]].size(); p++){
-				if(!s.count(out[in[i][j]][p]))
-					return false;
-			}
 		}
 	}
 	return true;
@@ -35,22 +30,18 @@ int main(){
 		freopen("UVa.11175.in", "r", stdin);
 		freopen("UVa.11175.out", "w", stdout);
 #endif
-	int T, kase = 0, a, b;
+	int T, kase = 0, a, b, m, k;
 	scanf("%d", &T);
 	while(T--){
+		memset(gt, 0, sizeof(gt));
+		memset(invgt, 0, sizeof(invgt));
 		scanf("%d%d", &m, &k);
-		for(int i = 0; i < maxn; i++){
-			in[i].clear(); 
-			out[i].clear();
-		}
-		for(int i = 0; i < k; i++){
+		while(k--){
 			scanf("%d%d", &a, &b);
-			out[a].push_back(b);
-			in[b].push_back(a);
+			g[a][gt[a]++] = b;
+			invg[b][invgt[b]++] = a;
 		}
-		printf("Case #%d: ", ++kase);
-		if(solve()) printf("Yes\n");
-		else printf("No\n");
+		printf("Case #%d: %s\n", ++kase, solve(m) ? "Yes":"No");
 	}
 	return 0;
 }
