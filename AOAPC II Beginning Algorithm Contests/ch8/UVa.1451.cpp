0233ed2788b6eea73689f9de042703a800337e40
Dalei Yang <daleiyang@gmail.com>
2020-08-21 16:12:48 +0800

    update UVa.1451.cpp

--- AOAPC II Beginning Algorithm Contests/ch8/UVa.1451.cpp
+++ AOAPC II Beginning Algorithm Contests/ch8/UVa.1451.cpp
@@ -1,44 +1,37 @@
-#include<cstdio>
+#include <cstdio>
 using namespace std;
-
-const int maxn = 100000 + 5;
-
-char s[maxn];
-int sum[maxn], p[maxn];
-
-int compare_avg(int x1, int x2, int x3, int x4){
-	return (sum[x2]-sum[x1-1])*(x4-(x3-1)) - (sum[x4]-sum[x3-1])*(x2-(x1-1));
+const int N=100005;
+int sm[N],q[N];
+char s[N];
+ 
+int sol(int x1,int x2,int x3,int x4)  {
+	int sol = (sm[x2]-sm[x1])*(x4-x3)-(sm[x4]-sm[x3])*(x2-x1);
+	return sol;
 }
 
-int main(){
+int main() {
 #ifdef LOCAL
 	freopen("UVa.1451.in", "r", stdin);
 	freopen("UVa.1451.out", "w", stdout);
 #endif
-	int T;
-	scanf("%d", &T);
-	while(T--){
-		int n, L;
-		scanf("%d%d%s", &n, &L, s+1);
-		
-		sum[0] = 0;
-		for(int i = 1; i <= n; i++) sum[i] = sum[i-1]+s[i]-'0';
-
-		int ansL = 1, ansR = L;
-		int i = 0, j = 0;
-		for(int t = L; t <= n; t++){
-			while(j-i > 1 && compare_avg(p[j-2], t-L, p[j-1], t-L) >= 0) j--;
-			p[j++] = t-L+1;
-			while(j-i > 1 && compare_avg(p[i], t, p[i+1], t) <= 0) i++;
-				
-			int c = compare_avg(p[i], t, ansL, ansR);
-			if(c > 0 || (c == 0 && t-p[i] < ansR- ansL)){
-				ansL = p[i];
-				ansR = t;
-			}
+	int n,l,T;
+ 
+	scanf("%d",&T);
+	while (T--) {
+		scanf("%d %d %s",&n,&l,s+1);
+		sm[0]=0;
+		for (int i=1;i<=n;i++) sm[i]=sm[i-1]+s[i]-'0';
+		int tl=0,tr=l;
+ 
+		int i=0,j=0;
+		for (int t=l;t<=n;t++) {
+			while(i<j-1&&sol(q[j-1],t-l,q[j-2],t-l)<0) j--;
+			q[j++]=t-l;
+			while(i<j-1&&sol(q[i+1],t,q[i],t)>=0) i++;
+ 
+			int tt=sol(q[i],t,tl,tr);
+			if (tt>0||tt==0&&(t-q[i]<tr-tl)) tr=t,tl=q[i];
 		}
-		printf("%d %d\n", ansL, ansR);
+		printf("%d %d\n",tl+1,tr);
 	}
-	return 0;
 }
-
